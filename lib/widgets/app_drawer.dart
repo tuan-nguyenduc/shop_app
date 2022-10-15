8c66d2d176bb77ffe2e29a3088fe225b34c6b05c
NguyenDucTuanUET <64391055+ductuanUET@users.noreply.github.com>
2022-10-15 22:45:39 +0700

    add app drawer

---
+++ lib/widgets/app_drawer.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/screens/user_products_screen.dart';
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          AppBar(
+            title: Text('Hello Friend!'),
+            automaticallyImplyLeading: false,
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.shop),
+            title: Text('Shop'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed('/');
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.payment),
+            title: Text('Orders'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed('/orders');
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.edit),
+            title: Text('Manage Products'),
+            onTap: () {
+              Navigator.of(context)
+                  .pushReplacementNamed(UserProductsScreen.routeName);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
